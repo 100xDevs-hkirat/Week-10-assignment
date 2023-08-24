b0fbe069fecc2cc7560b03b46b151e75a3e7c54a
shubhanshu.mani <shubhanshu.085@gmail.com>
2023-08-24 18:23:42 +0530

    send cookies in login route

--- week-10-ass/src/pages/api/admin/login.ts
+++ week-10-ass/src/pages/api/admin/login.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { AdminModel } from '@/db/index';
 import { connectDB } from '@/db/connect';
+import { generateToken } from '@/auth/session';
 // interface LoginRequest {
 //     username : string
 //     password : string
@@ -21,6 +22,8 @@ export default async function handler(
         return res.status(401).json({ message: 'username and password required' });
     const admin = await AdminModel.findOne({ username, password });
     if (admin) {
+        const token = generateToken(admin.username ,'ADMIN');
+        res.setHeader('Set-Cookie', 'auth-token=' + token +'; Max-Age=3600; Path=/');
         res.json({ message: 'Logged in successfully' });
     } else {
         res.status(403).json({ message: 'Invalid username or password' });
