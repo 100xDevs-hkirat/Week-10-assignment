7d3aa1fdce5949da5645449d745a5daf0bf3a8f0
shubhanshu.mani <shubhanshu.085@gmail.com>
2023-08-26 20:50:07 +0530

    add new course route

--- week-10-ass/src/pages/api/admin/courses.ts
+++ week-10-ass/src/pages/api/admin/courses.ts
@@ -3,21 +3,36 @@ import { validateToken } from '@/auth/session';
 import { Course, CourseModel } from '@/db';
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-interface CoursesResponse {
-    courses : Course[]
+interface CoursesListResponse {
+    courses: Course[]
 }
 
-interface AuthError{
-    message : string
+interface CourseAddResponse{
+    message : string,
+    courseId : string
+}
+
+interface AuthError {
+    message: string
 }
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<CoursesResponse | AuthError>
+    req: NextApiRequest,
+    res: NextApiResponse<CoursesListResponse | CourseAddResponse | AuthError>
 ) {
-    const cookieHeader = req.headers.cookie;
-    const admin = validateToken(cookieHeader);
-    if (!admin) return res.status(401).json({message : "please login"});
-    const courses : Course[] = await CourseModel.find({});
-    res.json({ courses });
+    if (req.method === 'GET') {
+        const cookieHeader = req.headers.cookie;
+        const admin = validateToken(cookieHeader);
+        if (!admin) return res.status(401).json({ message: "please login" });
+        const courses: Course[] = await CourseModel.find({});
+        res.json({ courses });
+    }
+    else if (req.method === 'POST') {
+        const course = new CourseModel(req.body);
+        await course.save();
+        res.json({ message: 'Course created successfully', courseId: course.id });
+    }
+    else {
+        res.status(405);
+    }
 }
